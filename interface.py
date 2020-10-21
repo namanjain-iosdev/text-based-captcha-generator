b8fb72e412f82aa811e97863eb9bc534ca3eb72c
naman2001jain <62708894+naman2001jain@users.noreply.github.com>
2020-10-21 23:57:44 +0530

    Update interface.py
    entry and submit button and captcha image

--- interface.py
+++ interface.py
@@ -1,12 +1,43 @@
 from tkinter import *
-from PIL import Image,ImageTk
-root = Tk()
-root.config(bg="#ffe6c8")
-root.geometry('900x900')
-root.minsize(400,300)
-page = Frame(root,bg="#ffe6c8").pack(fill=BOTH)
-f1=Frame(page,bg="#262729",height="700",width="700").pack(side=TOP)
-Frame(f1,bg="#ffe6c8",height="100",width="700").pack(side=TOP)
+from PIL import Image, ImageTk
+from func import *
+from tkinter import messagebox
+
+
+def chkauth():
+    if str(cap) == str(captcha_var.get()):
+        messagebox.showinfo("Information", "you entered right captcha!")
+    else:
+        messagebox.showwarning("Warning", "wrong captcha!!!")
+
+
+def retry():
+    cap = captcha_gen()
+    chkauth()
+
+
+main_screen = Tk()
+main_screen.config(bg="#ffe6c8")
+main_screen.geometry('700x600')
+main_screen.minsize(400, 300)
+
+l1 = Label(main_screen, text="Text Captcha", bg="#8c8272",height="0",width="700",
+           fg="white", font=('Times New Roman', 18))
+l1.pack(fill=Y)
+f1 = Frame(main_screen, bg="#262729", padx="100", pady="150")
+captcha_var = StringVar()
+captcha_entry = Entry(f1, textvariable=captcha_var)
+captcha_entry.grid(row=0, column=1)
+global cap
+cap = captcha_gen()
 cap_img = PhotoImage(file="cap_img.png")
-Label(f1,image = cap_img).pack()
-root.mainloop()
+Label(f1, image=cap_img).grid(row=0, column=3)
+
+
+submit_pic = PhotoImage(file='go_img.png')
+
+Button(f1, text="Submit", fg="black", command=chkauth,
+       image=submit_pic, padx="0", pady="0").grid(row=1, column=1)
+
+f1.pack()
+main_screen.mainloop()
