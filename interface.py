67eea1fbb96f2fbfd96e5f18a3568a85951c2184
naman2001jain <62708894+naman2001jain@users.noreply.github.com>
2020-10-22 17:58:06 +0530

    Update interface.py
    interface designed updated

--- interface.py
+++ interface.py
@@ -3,41 +3,54 @@ from PIL import Image, ImageTk
 from func import *
 from tkinter import messagebox
 
-
 def chkauth():
     if str(cap) == str(captcha_var.get()):
-        messagebox.showinfo("Information", "you entered right captcha!")
+        l1.configure(background="green",text="successful")
     else:
-        messagebox.showwarning("Warning", "wrong captcha!!!")
+        l1.configure(background="red",text="invalid")
 
 
-def retry():
+
+def login_screen():
+    main_screen = Tk()
+    main_screen.config(bg="#ffe6c8")
+    main_screen.geometry('700x550')
+    main_screen.minsize(400, 300)
+    main_screen.title("Text Captcha")
+    #main_screen.wm_iconbitmap("1.icn")
+    global l1
+    l1 = Label(main_screen, text="Status", bg="#8c8272",fg="white", font=('Times New Roman', 18))
+    l1.pack(fill=X,side=TOP)
+
+    f1 = Frame(main_screen, bg="#262729", padx="100", pady="150")
+    f1.pack()
+    global user_var
+    global pass_var
+    global captcha_var
+    user_var = StringVar(main_screen,value="user name")
+    pass_var = StringVar(main_screen,value="password")
+    captcha_var = StringVar()
+
+    user_entry = Entry(f1, textvariable=user_var)
+    user_entry.pack(side=TOP,anchor="nw",pady="5")
+
+
+    pass_entry = Entry(f1, textvariable=pass_var,show="*")
+    pass_entry.pack(side=TOP,anchor="nw",pady="5")
+
+    
+    
+    captcha_entry = Entry(f1, textvariable=captcha_var)
+    captcha_entry.pack(side=TOP,pady="5")
+    global cap
     cap = captcha_gen()
-    chkauth()
+    cap_img = PhotoImage(file="cap_img.png")
+    Label(f1, image=cap_img).pack(pady="5")
+    submit_pic = PhotoImage(file='go_img.png')
+    retry_pic = PhotoImage(file='retry_img.png')
+    Button(f1, fg="black", command=chkauth,image=retry_pic, padx="0", pady="0").pack(side=RIGHT)
+    Button(f1, text="Submit", fg="black", command=chkauth,image=submit_pic, padx="0", pady="0").pack(side=BOTTOM)
+    
+        
 
-
-main_screen = Tk()
-main_screen.config(bg="#ffe6c8")
-main_screen.geometry('700x600')
-main_screen.minsize(400, 300)
-
-l1 = Label(main_screen, text="Text Captcha", bg="#8c8272",height="0",width="700",
-           fg="white", font=('Times New Roman', 18))
-l1.pack(fill=Y)
-f1 = Frame(main_screen, bg="#262729", padx="100", pady="150")
-captcha_var = StringVar()
-captcha_entry = Entry(f1, textvariable=captcha_var)
-captcha_entry.grid(row=0, column=1)
-global cap
-cap = captcha_gen()
-cap_img = PhotoImage(file="cap_img.png")
-Label(f1, image=cap_img).grid(row=0, column=3)
-
-
-submit_pic = PhotoImage(file='go_img.png')
-
-Button(f1, text="Submit", fg="black", command=chkauth,
-       image=submit_pic, padx="0", pady="0").grid(row=1, column=1)
-
-f1.pack()
-main_screen.mainloop()
+    main_screen.mainloop()
