56211e09b339ce214d03be2cfb38e32543b55a44
naman2001jain <62708894+naman2001jain@users.noreply.github.com>
2020-10-30 08:48:48 +0530

    Update __init__.py
    updated file

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -1,14 +1,6 @@
 # coding: utf-8
 
-"""
-    Captcha
-    ~~~~~~~
 
-    A captcha library that generates audio and image CAPTCHAs.
-
-    :copyright: (c) 2015 - 2018 by Hsiaoming Yang.
-    :license: BSD, see LICENSE for more details.
-"""
 
 __version__ = '0.3'
 __author__ = 'Hsiaoming Yang <me@lepture.com>'
